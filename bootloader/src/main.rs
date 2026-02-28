0187bb9c268131f1b24c31f1e23633e0ad7b77f1
BrokenC1oud <jgbsxx20130315@gmail.com>
2026-02-28 11:33:15 +0800

    initial commit

---
+++ bootloader/src/main.rs
@@ -0,0 +1,66 @@
+use uefi::{
+    CStr16, Identify, Result,
+    boot::{self, AllocateType, MemoryType, SearchType},
+    println,
+    proto::media::{
+        file::{File, FileAttribute, FileInfo, FileMode, FileType},
+        fs::SimpleFileSystem,
+    },
+};
+
+#[uefi::entry]
+fn main() -> uefi::Status {
+    bootloader_inner().unwrap();
+
+    loop {}
+
+    unreachable!()
+}
+
+fn bootloader_inner() -> Result<()> {
+    let kernel_slice = read_file("kernel")?;
+}
+
+fn read_file(filename: &str) -> Result<&[u8]> {
+    let simple_file_system_protocol_handle =
+        *boot::locate_handle_buffer(SearchType::ByProtocol(&SimpleFileSystem::GUID))?
+            .first()
+            .expect("Failed to get first handle");
+    let mut simple_file_system_protocol =
+        boot::open_protocol_exclusive::<SimpleFileSystem>(simple_file_system_protocol_handle)?;
+    let mut simple_file_system = simple_file_system_protocol.open_volume()?;
+
+    // I don't care cause it's always 'kernel', use cstr16! macro!
+    let mut buffer = [0u16; 8];
+    let mut file_handle = simple_file_system.open(
+        CStr16::from_str_with_buf(filename, &mut buffer).unwrap(),
+        FileMode::Read,
+        FileAttribute::READ_ONLY,
+    )?;
+
+    let mut buffer = [0u8; 1024];
+    let file_info = file_handle
+        .get_info::<FileInfo>(&mut buffer)
+        .expect("failed to get file info");
+    let mut file = match file_handle.into_type()? {
+        FileType::Regular(file) => file,
+        FileType::Dir(_) => unimplemented!("file expected: {}", filename),
+    };
+    let file_ptr = boot::allocate_pages(
+        AllocateType::AnyPages,
+        MemoryType::LOADER_DATA,
+        file_info.file_size() as usize,
+    )?
+    .as_ptr();
+
+    println!("file loading into 0x{:x}", file_ptr as usize);
+
+    unsafe { file_ptr.write_bytes(0, file_info.file_size() as usize) }
+
+    let file_slice =
+        unsafe { core::slice::from_raw_parts_mut(file_ptr, file_info.file_size() as usize) };
+
+    file.read(file_slice)?;
+
+    Ok(file_slice)
+}
---
+++ build.rs
@@ -0,0 +1,77 @@
+use std::{path::PathBuf, process::Command};
+
+use ovmf_prebuilt::{Arch, FileType, Prebuilt, Source};
+
+fn main() {
+    println!("cargo::rerun-if-changed=build.rs");
+    fetch_ovmf();
+    build_bootloader();
+    // build_kernel();
+}
+
+fn fetch_ovmf() {
+    let ovmf = Prebuilt::fetch(Source::LATEST, "vm/ovmf").expect("Failed to fetch prebuilt");
+    println!(
+        "cargo::rustc-env=OVMF_CODE={}",
+        ovmf.get_file(Arch::X64, FileType::Code).display()
+    );
+    println!(
+        "cargo::rustc-env=OVMF_VARS={}",
+        ovmf.get_file(Arch::X64, FileType::Vars).display()
+    );
+}
+
+fn build_bootloader() {
+    let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    let cargo = std::env::var("CARGO").unwrap_or_else(|_| "cargo".into());
+
+    println!("cargo::rerun-if-changed=bootloader");
+
+    let mut cmd = Command::new(cargo);
+    cmd.arg("build");
+    cmd.arg("-p").arg("bootloader");
+    cmd.arg("--target").arg("x86_64-unknown-uefi");
+    cmd.arg("--target-dir").arg(&out_dir);
+    cmd.arg("--release");
+
+    let status = cmd.status().expect("failed to build bootloader");
+
+    if status.success() {
+        let bootloader = out_dir
+            .join("x86_64-unknown-uefi")
+            .join("release")
+            .join("bootloader.efi");
+        assert!(bootloader.exists(), "bootloader expected");
+        println!("cargo::rustc-env=BOOTLOADER_OUT={}", bootloader.display());
+    } else {
+        panic!("failed to build bootloader");
+    }
+}
+
+fn build_kernel() {
+    let out_dir = PathBuf::from(std::env::var("OUT_DIR").unwrap());
+    let cargo = std::env::var("CARGO").unwrap_or_else(|_| "cargo".into());
+
+    println!("cargo::rerun-if-changed=kernel");
+
+    let mut cmd = Command::new(cargo);
+    cmd.arg("build");
+    cmd.arg("-p").arg("kernel");
+    cmd.arg("--target").arg("x86_64-unknown-none");
+    cmd.arg("--target-dir").arg(&out_dir);
+    cmd.arg("--release");
+
+    let status = cmd.status().expect("failed to build kernel");
+
+    if status.success() {
+        let kernel = out_dir
+            .join("x86_64-unknown-none")
+            .join("release")
+            .join("kernel");
+        println!("{}", kernel.display());
+        assert!(kernel.exists(), "kernel expected");
+        println!("cargo::rustc-env=KERNEL_OUT={}", kernel.display());
+    } else {
+        panic!("failed to build kernel");
+    }
+}
