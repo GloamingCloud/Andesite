11aa2bf5b5d38ab0c5b780674061ce39815a3350
BrokenC1oud <jgbsxx20130315@gmail.com>
2026-07-08 20:39:41 +0800

    bit

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,16 +1,15 @@
 #![no_std]
 #![no_main]
 
-use core::panic::PanicInfo;
-
 use common::KernelParameters;
 
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(_info: &core::panic::PanicInfo) -> ! {
     loop {}
 }
 
 #[unsafe(no_mangle)]
 pub extern "sysv64" fn _start(kernel_parameters: KernelParameters) -> usize {
-    return kernel_parameters.sth;
+    kernel_parameters.sth
 }
